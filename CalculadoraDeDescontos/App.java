dd6a50ebe3d2f52ebaa2a301650229adac56507d
EllysonPR <ellysonparisi@gmail.com>
2023-02-17 19:31:05 -0300

    17/02//2023

---
+++ CalculadoraDeDescontos/App.java
@@ -0,0 +1,10 @@
+package CalculadoraGastoViagem.CalculadoraDeDescontos;
+
+public class App {
+    public static void main(String[] args) {
+        CalcularDesconto cDesconto = new CalcularDesconto();
+        cDesconto.calcular();
+        cDesconto.calcular30();
+    }
+    
+}
